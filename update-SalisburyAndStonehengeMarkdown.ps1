cd75e7a8dba3652ecaf8a76d694b8ce8174155fc
mattypenny <mattypenny@outlook.com>
2016-03-14 08:33:26 +0000

    tidyed up the individual changes

--- update-SalisburyAndStonehengeMarkdown.ps1
+++ update-SalisburyAndStonehengeMarkdown.ps1
@@ -18,12 +18,6 @@ function update-SasIndividualFiles
     [OutputType([int])]
     Param
     (
-        # Param1 help description
-        [Parameter(Mandatory=$true,
-                   ValueFromPipelineByPropertyName=$true,
-                   Position=0)]
-        $Param1,
-
         # Param2 help description
         [int]
         $Param2
@@ -32,29 +26,30 @@ function update-SasIndividualFiles
    
     cd D:\hugo\sites\example.com\content\on-this-day
 
-    update-HugoPageToDraft -page march.md                                                                                                                                
-    update-HugoPageWeight -page march-1627-the-plague-reaches-salisbury.md -weight "0300"                                                                                              
-    rename dickens-and-salisbury-uk.md 7th-February-1812-Charles-Dickens-birthday.md
-    update-HugoPageWeight -page 7th-February-1812-Charles-Dickens-birthday.md "0207"                                                                                                            
-    update-HugoPageToDraft february.md                                                                                                                             
-    rename 16-february-1972-free-play-at-salisbury-city-hall.md   16th-february-1972-free-play-at-salisbury-city-hall.md                                                                                 
-    update-HugoPageToDraft january.md                                                                                                                              
-    rename 8122-2.md "5th-january-1818-prince-leopold-future-king-of-belgium-visits-salisbury.md"                                                                                                                           
-    rename  come-dine-with-me-in-salisbury.md 24th-september-20120come-dine-with-me-in-salisbury.md
-    rename 24-november-1979-salisbury-fc-play-millwall-in-fa-cup-3rd-round.md  24th-november-1979-salisbury-fc-play-millwall-in-fa-cup-3rd-round.md                                                                     
-    update-HugoPageWeight tuesday-after-12th-night-salisbury-cattle-and-wool-fair.md -weight "0108"                                                                              
-    rename 15-jul-1965-them-play-salisbury-city-hall.md   15th-jul-1965-them-play-salisbury-city-hall.md                                                                                          
-    rename 26-sept-1963-gene-vincent-plays-in-salisbury.md  26th-sept-1963-gene-vincent-plays-in-salisbury.md                                                                                        
-    update-HugoPageWeight october-2003-the-tailor-of-salisbury-published.md                                                                                       
-    update-HugoPageWeight first-thursday-in-may-1723-new-prize-at-salisbury-races.md                                                                              
-    update-HugoPageWeight end-of-july-1665-charles-ii-comes-to-salisbury-to-escape-the-plague.md                                                                  
-    rename august-10th-1871-crown-prince-of-prussia-victoria-the-princess-royal-and-the-future-kaiser-willhelm-ii-visit-salisbury-and-stonehenge.md
-    rename 22-september-1766-salisbury-people-force-famers-to-sell-at-a-fair-price.md 22nd-september-1766-salisbury-people-force-famers-to-sell-at-a-fair-price.md                                                             
-    rename 8439-2.md 15th-october-king-charles-enters-Salisbury-with-army.md                                                                                                                   
-    rename 8443-2.md 28th-september-1625-king-charles-I-visited-salisbury.md                                                             
+
                                                                 
-    rename 21-december-1971-led-zeppelin-play-salisbury-city-hall.md    21st-december-1971-led-zeppelin-play-salisbury-city-hall.md                                                                           
-    rename 27-november-1736-issue-no-1-of-william-collins-salisbury-journal.md 27th-november-1736-issue-no-1-of-william-collins-salisbury-journal.md                                                                     
+    rename-item  come-dine-with-me-in-salisbury.md 24th-september-20120come-dine-with-me-in-salisbury.md
+    rename-item 15-jul-1965-them-play-salisbury-city-hall.md   15th-jul-1965-them-play-salisbury-city-hall.md                                                                                          
+    rename-item 16-february-1972-free-play-at-salisbury-city-hall.md   16th-february-1972-free-play-at-salisbury-city-hall.md                                                                                 
+    rename-item 21-december-1971-led-zeppelin-play-salisbury-city-hall.md    21st-december-1971-led-zeppelin-play-salisbury-city-hall.md                                                                           
+    rename-item 22-september-1766-salisbury-people-force-famers-to-sell-at-a-fair-price.md 22nd-september-1766-salisbury-people-force-famers-to-sell-at-a-fair-price.md                                                             
+    rename-item 24-november-1979-salisbury-fc-play-millwall-in-fa-cup-3rd-round.md  24th-november-1979-salisbury-fc-play-millwall-in-fa-cup-3rd-round.md                                                                     
+    rename-item 26-sept-1963-gene-vincent-plays-in-salisbury.md  26th-sept-1963-gene-vincent-plays-in-salisbury.md                                                                                        
+    rename-item 27-november-1736-issue-no-1-of-william-collins-salisbury-journal.md 27th-november-1736-issue-no-1-of-william-collins-salisbury-journal.md                                                                     
+    rename-item 8122-2.md "5th-january-1818-prince-leopold-future-king-of-belgium-visits-salisbury.md"                                                                                                                           
+    rename-item 8439-2.md 15th-october-king-charles-enters-Salisbury-with-army.md                                                                                                                   
+    rename-item 8443-2.md 28th-september-1625-king-charles-I-visited-salisbury.md                                                             
+    # rename-item august-10th-1871-crown-prince-of-prussia-victoria-the-princess-royal-and-the-future-kaiser-willhelm-ii-visit-salisbury-and-stonehenge.md
+    rename-item dickens-and-salisbury-uk.md 7th-February-1812-Charles-Dickens-birthday.md
+    update-HugoPageFrontMatter -file 7th-February-1812-Charles-Dickens-birthday.md -key weight -value "0207"                                                                                                            
+    update-HugoPageFrontMatter -file february.md -key draft -value No
+    update-HugoPageFrontMatter -file january.md -key draft -value No 
+    update-HugoPageFrontMatter -file march-1627-the-plague-reaches-salisbury.md -key weight -value "0300"                                                                                              
+    update-HugoPageFrontMatter -file march.md -Value No
+    update-HugoPageFrontMatter end-of-july-1665-charles-ii-comes-to-salisbury-to-escape-the-plague.md  -key weight -value  "0732"                                                                
+    update-HugoPageFrontMatter first-thursday-in-may-1723-new-prize-at-salisbury-races.md -key weight -value  "0005" 
+    update-HugoPageFrontMatter october-2003-the-tailor-of-salisbury-published.md -key weight -value  "0010"
+    update-HugoPageFrontMatter tuesday-after-12th-night-salisbury-cattle-and-wool-fair.md -key weight -value "0108"                                                                              
 
 }
 
